f03cd5375cf58f70df6bc1f17f30366cc22a2b05
yaxh.xv <yashnawgiri45@gmail.com>
2024-10-10 10:16:06 +0530

    test commit

---
+++ api/server.js
@@ -0,0 +1,24 @@
+import Hapi from 'hapi';
+
+const server = new Hapi.Server({
+  host: '127.0.0.1',
+  port: '8080',
+  routes: {
+    cors: { origin: 'ignore' },
+  },
+});
+
+async function main() {
+  await server.register([{
+    plugin: require('./shifts-mock-api'),
+    routes: { prefix: '/shifts' },
+  }]);
+
+  await server.start();
+
+  console.info(`✅  API server is listening at ${server.info.uri.toLowerCase()}`);
+}
+
+main();
+
+//this is a test message
